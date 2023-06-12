8c01a72b36bb49dc5fa7a8462d2cd7cd41335c33
Ykylas <albertov.sasha@mail.ru>
2023-06-12 13:18:46 +0600

    button

--- tutorialStoryBoard2.0/ViewController.swift
+++ tutorialStoryBoard2.0/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var continueButton: UIButton!
+    @IBOutlet var alaqanAppLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        alaqanAppLabel.isHidden = true
+        continueButton.layer.cornerRadius = 20
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func makeButtonAction(_ sender: Any) {
+        if alaqanAppLabel.isHidden {
+            alaqanAppLabel.isHidden = false
+            continueButton.setTitle("Hide text", for: .normal)
+        } else {
+            alaqanAppLabel.isHidden = true
+            continueButton.setTitle("Show text", for: .normal)
+        }
+    }
+    
 }
 
